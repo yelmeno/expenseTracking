954915832eeb056c4b524521cfb2694d217ea7a7
yelmeno <111025483+yelmeno@users.noreply.github.com>
2023-07-07 21:21:40 +0300

    first commit

---
+++ src/main/java/com/innova/expenseTracking/ExpenseTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.innova.expenseTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innova/expenseTracking/ExpenseTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.expenseTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
